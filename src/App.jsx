3039826c3d3eb1e13b600a545300e64db699cb3d
lppduy <lephamphuongduy@gmail.com>
2023-09-06 18:49:28 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav className="navbar">Navbar content</nav>
+
+      <main>Main content</main>
+    </div>
+  );
+};
+
+export default App;
